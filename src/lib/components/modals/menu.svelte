e6361952378e9f4cd4c82857d5ab0b5fafe49f02
Ashok Kumar Pachauri <pachauriashokkumar@gmail.com>
2025-04-03 05:06:32 +0530

    Initial Commit

---
+++ src/lib/components/modals/menu.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Network from "../network.svelte";
+</script>
+
+{#each [{ name: "Home", url: "/" }, { name: "About IM WEB PROS", url: "https://imwebpros.com" }] as { name, url }}
+    <a
+        href={url}
+        data-sveltekit-reload
+        class="btn-ghost btn w-full">{name}</a
+    >
+{/each}
+<Network />
